6da192a5d61abf7bf4463b2a8a97568715ba9625
Markel Sanz Ausin <msanzau@ncsu.edu>
2020-04-26 15:10:04 -0400

    evaluate_atari.sh script modified.

--- pytorch/experiment_scripts/evaluate_atari.sh
+++ pytorch/experiment_scripts/evaluate_atari.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES="3" python evaluate.py --env=BreakoutNoFrameskip-v4 --agent=DQN --double_q=1 --dueling=1 --num_episodes=50
+CUDA_VISIBLE_DEVICES="0" python evaluate.py --env=AmidarNoFrameskip-v4 --agent=DQN --double_q=0 --dueling=0 --num_episodes=200
